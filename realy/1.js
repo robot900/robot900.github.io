3fee6935a543b114f6715d99d90b442bc36b37ee
robot900 <83409722+robot900@users.noreply.github.com>
2021-05-03 10:53:43 +0500

    Delete 1.js

--- realy/1.js
+++
@@ -1,9 +0,0 @@
-function goin(){
-	var userid = document.getElementById("userid").value;
-	var userpass = document.getElementById("userpass").value;
-	if (userid == "Vasya"){
-		if (userpass == "123456789"){
-			window.location.href="real.html";
-		}
-	}
-}
